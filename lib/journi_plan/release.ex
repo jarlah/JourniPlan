0fe924cd040f17c389574aed62bed77584704e27
Jarl André Hübenthal <jarlah@protonmail.com>
2024-09-28 09:41:49 +0200

    init evenstore in migration release command

--- lib/journi_plan/release.ex
+++ lib/journi_plan/release.ex
@@ -8,6 +8,12 @@ defmodule JourniPlan.Release do
   def migrate do
     load_app()
 
+    {:ok, _} = Application.ensure_all_started(:postgrex)
+    {:ok, _} = Application.ensure_all_started(:ssl)
+    config = JourniPlan.EventStore.config()
+    :ok = EventStore.Tasks.Create.exec(config, [])
+    :ok = EventStore.Tasks.Init.exec(config, [])
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
@@ -26,15 +32,4 @@ defmodule JourniPlan.Release do
     Application.load(@app)
   end
 
-  def init_event_store do
-    {:ok, _} = Application.ensure_all_started(:postgrex)
-    {:ok, _} = Application.ensure_all_started(:ssl)
-
-    :ok = Application.load(@app)
-
-    config = JourniPlan.EventStore.config()
-
-    :ok = EventStore.Tasks.Create.exec(config, [])
-    :ok = EventStore.Tasks.Init.exec(config, [])
-  end
 end
